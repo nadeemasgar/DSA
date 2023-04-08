006db69dd11ab997c74f2e21ccbd9049cef198c4
Nadeem <nadeemasgar18@gmail.com>
2023-04-08 15:11:21 +0530

    more codes

--- 8. Recursion/5. Permutations.java
+++ 8. Recursion/5. Permutations.java
@@ -5,7 +5,7 @@
 
 import java.util.*;
 
-class Solution {
+class Solution1 {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
@@ -30,4 +30,40 @@ class Solution {
             }
         }
     }
+}
+
+
+// Approach 2 - Using swappings for space optimization
+
+class Solution2 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        recurPermute(0, nums, ans);
+        return ans;
+    }
+
+    public void recurPermute(int idx, int[] nums, List<List<Integer>> ans) {
+        if(idx == nums.length) {
+            // copy the ds to ans
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
+
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i = idx; i < nums.length; i++) {
+            swap(i, idx, nums);
+            recurPermute(idx + 1, nums, ans);
+            swap(i, idx, nums);
+        }
+    }
+
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    } 
 }
