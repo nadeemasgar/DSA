09295b71f087d1d2f3cca4b70d0f2a781c40b942
Nadeem <nadeemasgar18@gmail.com>
2023-05-21 00:54:17 +0530

    more codes

---
+++ 5 System Design code/2. Design System/1. Snake & Ladder/SnakeAndLadder/src/Player.java
@@ -0,0 +1,14 @@
+
+public class Player {
+	private String playerName;
+	private int id;
+	
+	Player(String playerName, int id) {
+		this.playerName = playerName;
+		this.id = id;
+	}
+	
+	String getPlayerName() {
+		return playerName;
+	}
+}
